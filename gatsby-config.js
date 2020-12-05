2eae42a6a1607e741d6b5ab374c7474822f08ec9
Dave Lu <davelu@yelp.com>
2020-12-04 17:11:50 -0800

    Hook up my roam graph

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,22 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
 module.exports = {
   plugins: [
     {
       resolve: `gatsby-theme-garden`,
       options: {
         contentPath: `${__dirname}/content/garden`,
-        rootNote: `/hello`,
+        rootNote: `/Dave's-Public-Notes`,
+        roamUrl: `https://roamresearch.com/#/app/DaveLuPublic`,
+        roamEmail: process.env.ROAM_RESEARCH_EMAIL,
+        roamPassword: process.env.ROAM_RESEARCH_PASSWORD,
       },
     },
   ],
   siteMetadata: {
-    title: `Site title`,
+    title: `Dave's Public Notes`,
+    description: `An assortment of half-baked ideas and thoughts`,
   },
 }
